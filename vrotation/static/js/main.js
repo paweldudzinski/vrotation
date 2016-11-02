a1dbfebcf54b6b03377695900d4897701c91d270
Paweł Dudziński <pawel.d@webinterpret.com>
2016-11-02 16:00:08 +0100

    JS for adding players numbers

--- vrotation/static/js/main.js
+++ vrotation/static/js/main.js
@@ -1,1 +1,14 @@
+$(document).ready(function(){
 
+	enter_number('.left-player');
+	enter_number('.right-player');
+
+});
+
+function enter_number(selector) {
+	$(selector).click(function() {
+		var ID = $(this).attr('id');
+		var player = prompt("Please enter player number", "");
+		$('#'+ID+'nr').html(player);
+	});
+}
