e92a410ae0b908fb618d8be5885148023742a0c2
Paweł Dudziński <pawel.d@webinterpret.com>
2016-11-03 15:40:13 +0100

    Add rotation

--- vrotation/static/js/main.js
+++ vrotation/static/js/main.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 
 	enter_number('.left-player');
 	enter_number('.right-player');
+	
+	$('.rotate').click(function() {
+		ID = $(this).attr('id');
+		rotate(ID)
+	});
 
 });
 
@@ -11,4 +16,20 @@ function enter_number(selector) {
 		var player = prompt("Please enter player number", "");
 		$('#'+ID+'nr').html(player);
 	});
+}
+
+function rotate(selector) {
+	var side = selector.split('-')[0][0];
+	var N = 6;
+	var pos5 = $("#"+side+"p"+N+"nr").html();
+	for (var i=1; i<=N; i++) {
+		var player_selector = "#"+side+"p"+i+"nr";
+		var current_number = $(player_selector).html();
+		if (i == 1) {
+			$("#"+side+"p"+N+"nr").html(current_number);
+		} else {
+			$("#"+side+"p"+(i-1)+"nr").html(current_number);
+		}
+	}
+	$("#"+side+"p"+(N-1)+"nr").html(pos5);
 }
